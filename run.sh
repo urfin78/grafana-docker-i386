904ea20447e48bbeff2419bb2eda030f25e50bea
urfin78 <urfin78@gmail.com>
2019-02-27 02:10:48 +0100

    cleared whitespaces

--- run.sh
+++ run.sh
@@ -78,13 +78,13 @@ if [ ! -z "${GF_INSTALL_PLUGINS}" ]; then
   done
 fi
 
-exec grafana-server                                         \   
-  --homepath="$GF_PATHS_HOME"                               \   
-  --config="$GF_PATHS_CONFIG"                               \   
-  --packaging=docker                                        \   
-  "$@"                                                      \   
-  cfg:default.log.mode="console"                            \   
-  cfg:default.paths.data="$GF_PATHS_DATA"                   \   
-  cfg:default.paths.logs="$GF_PATHS_LOGS"                   \   
-  cfg:default.paths.plugins="$GF_PATHS_PLUGINS"             \   
-  cfg:default.paths.provisioning="$GF_PATHS_PROVISIONING"
+exec grafana-server \
+  --homepath="$GF_PATHS_HOME" \
+  --config="$GF_PATHS_CONFIG" \
+  --packaging=docker \
+  "$@" \
+  cfg:default.log.mode="console" \
+  cfg:default.paths.data="$GF_PATHS_DATA" \
+  cfg:default.paths.logs="$GF_PATHS_LOGS" \
+  cfg:default.paths.plugins="$GF_PATHS_PLUGINS" \
+  cfg:default.paths.provisioning="$GF_PATHS_PROVISIONING"1
